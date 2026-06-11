4486a277ef3c51c9a556ddc7a1ad958f090b18ff
rdc0312 <1300859139@qq.com>
2026-06-12 06:04:45 +0800

    style: reorder pub use statements for consistency in mod.rs

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -2,8 +2,8 @@ mod render;
 
 use ratatui::layout::Size;
 use ratatui::widgets::StatefulWidget;
-pub use render::RenderLoop;
 pub(crate) use render::CrosstermEventSource;
+pub use render::RenderLoop;
 use tui_scrollview::{ScrollView, ScrollViewState, ScrollbarVisibility};
 
 use crate::event::{EventContext, EventListener, EventType, ListenerId};
